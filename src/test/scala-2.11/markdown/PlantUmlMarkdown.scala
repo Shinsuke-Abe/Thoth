d2cdcff329256066b6e3fd2c7b6fe9254f134cce
Shinsuke Abe <mao.instantlife@gmail.com>
2015-10-02 06:21:31 +0900

    カスタムマークダウンのテスト観点を追加

---
+++ src/test/scala-2.11/markdown/PlantUmlMarkdown.scala
@@ -0,0 +1,16 @@
+package markdown
+
+/**
+ * @author mao.instantlife at gmail.com
+ */
+
+import org.specs2.mutable._
+
+class PlantUmlMarkdown extends Specification {
+  // TODO 記法は &[代替テキスト](pumlファイルのパス "タイトル") とする
+  // TODO 代替テキストは省略可能
+  // TODO タイトルは省略可能
+  // TODO pumlがルールに従って存在しない場合は例外をスロー
+  // TODO pumlがルール通りに存在する場合はイメージのマークダウンに書き換える
+
+}
