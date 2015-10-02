57727e9b704be68ce66c7918aa3ca2229821566f
Shinsuke-Abe <mao.instantlife@gmail.com>
2015-10-02 09:15:07 +0900

    クラス名を変更

--- src/test/scala-2.11/markdown/ThothCustomMarkdownParserSpec.scala
+++ src/test/scala-2.11/markdown/ThothCustomMarkdownParserSpec.scala
@@ -6,11 +6,12 @@ package markdown
 
 import org.specs2.mutable._
 
-class PlantUmlMarkdown extends Specification {
+class ThothCustomMarkdownParserSpec extends Specification {
   // TODO 記法は &[代替テキスト](pumlファイルのパス "タイトル") とする
   // TODO 代替テキストは省略可能
   // TODO タイトルは省略可能
   // TODO pumlがルールに従って存在しない場合は例外をスロー
   // TODO pumlがルール通りに存在する場合はイメージのマークダウンに書き換える
+
 
 }
