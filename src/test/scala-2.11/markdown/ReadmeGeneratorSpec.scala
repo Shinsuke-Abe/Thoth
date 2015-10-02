86a9f03b8d60f28a0d60e7491b03cf37ae95e578
Shinsuke-Abe <mao.instantlife@gmail.com>
2015-10-02 18:52:36 +0900

    README生成のためのテスト項目整理

---
+++ src/test/scala-2.11/markdown/ReadmeGeneratorSpec.scala
@@ -0,0 +1,12 @@
+package markdown
+
+import org.specs2.mutable._
+
+/**
+ * Created by shinsuke-abe on 2015/10/02.
+ */
+class ReadmeGeneratorSpec extends Specification {
+  // TODO ファイル内にH1がある時はリンクテキストにする
+  // TODO ファイル名にH1がない時はファイル名のみとする
+  // TODO README.mdを生成する
+}
