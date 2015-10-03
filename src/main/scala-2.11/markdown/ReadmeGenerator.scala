1e4d4b4521eb0b53813541b353316a5e70ef59c1
Shinsuke Abe <mao.instantlife@gmail.com>
2015-10-03 15:19:03 +0900

    ドキュメントのリンク作成メソッド実装

---
+++ src/main/scala-2.11/markdown/ReadmeGenerator.scala
@@ -0,0 +1,21 @@
+package markdown
+
+import ammonite.ops._
+
+/**
+ * @author mao.instantlife at gmail.com
+ */
+object ReadmeGenerator {
+  def createDocumentLink(file: Path) = (read! file).split("\n").toList.find(_.startsWith("# ")) match {
+    case Some(headerline) => {
+      val title = headerline.replaceAll("# ", "")
+
+      s"[$title](${(stat! file).name})"
+    }
+    case None => {
+      val fileName = (stat! file).name
+
+      s"[$fileName]($fileName)"
+    }
+  }
+}
--- src/test/scala-2.11/markdown/ReadmeGeneratorSpec.scala
+++ src/test/scala-2.11/markdown/ReadmeGeneratorSpec.scala
@@ -1,12 +1,63 @@
 package markdown
 
+import java.io.IOException
+import java.nio.file.NoSuchFileException
+
 import org.specs2.mutable._
+import ammonite.ops._
 
 /**
  * Created by shinsuke-abe on 2015/10/02.
  */
 class ReadmeGeneratorSpec extends Specification {
-  // TODO ファイル内にH1がある時はリンクテキストにする
-  // TODO ファイル名にH1がない時はファイル名のみとする
+
+  val targetBaseDir = cwd/'target/"scala-2.11"/"test-classes"/'ReadmeGeneratorSpec
+
+  "createDocumentLink" >> {
+    def testSequence(targetFile: Path, expected: String) = {
+      ReadmeGenerator.createDocumentLink(targetFile) must equalTo(expected)
+    }
+
+    "ファイル内にH1がある時はリンクテキストにする" >> testSequence(
+      targetBaseDir/"hastitleheader.md",
+      "[テストタイトル](hastitleheader.md)")
+
+    "ファイル名にH1がない時はファイル名のみとする" >> testSequence(
+      targetBaseDir/"withouttitleheader.md",
+      "[withouttitleheader.md](withouttitleheader.md)")
+
+    "ファイル内にH1が二つ以上ある場合は最初のH1を採用する" >> testSequence(
+      targetBaseDir/"hasmultipletitleheader.md",
+      "[第１章](hasmultipletitleheader.md)")
+
+    "ファイル内のH1が最初にない場合でも取得することができる" >> testSequence(
+      targetBaseDir/"hastitleheaderonsecond.md",
+      "[ドキュメントタイトル](hastitleheaderonsecond.md)")
+
+    "指定されたファイルが存在しない場合は例外をスローする" >> {
+      ReadmeGenerator.createDocumentLink(targetBaseDir/"notfound.md") must
+        throwA[NoSuchFileException]
+    }
+
+    "指定されたパスがディレクトリの場合は例外をスローする" >> {
+      ReadmeGenerator.createDocumentLink(targetBaseDir) must
+        throwA[IOException]
+    }
+  }
+
+  // TODO サブディレクトリがある場合は
   // TODO README.mdを生成する
+
+  // フォーマット
+  // # ディレクトリ名目次
+  //
+  // ## ドキュメント一覧
+  //
+  // * [H1タイトル](リンク)
+  // * ...
+  //
+  // ## サブドキュメント一覧
+  //
+  // * [ディレクトリ名](READMEへのリンク)
+  // * ...
 }
