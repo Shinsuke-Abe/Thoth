93b47f2737fdeeb14020f4c0f7533ddb7bba822d
Shinsuke Abe <mao.instantlife@gmail.com>
2015-10-04 20:42:15 +0900

    Markdownの出力ロジックを実装

--- src/main/scala-2.11/ThothMain.scala
+++ src/main/scala-2.11/ThothMain.scala
@@ -1,6 +1,7 @@
 import ammonite.ops._
-import dirs.{Markdown, OutputPathCreator, DocDirectoryParser, OutputType}
+import dirs._
 import markdown.{ReadmeGenerator, ThothCustomMarkdownParser}
+import pandoc.PandocExecutor
 import plantuml.PlantUmlImageGenerator
 
 /**
@@ -17,27 +18,30 @@ object ThothMain extends App {
   } else false
 
   implicit val inputBaseDir = Path(inputDir)
-  val inputDirectories = DocDirectoryParser(inputDir) // TODO 引数の型をPathに変更した方がいい
+  generateDocuments(DocDirectoryParser(inputDir)) // TODO 引数の型をPathに変更した方がいい
+  
+  def generateDocuments(docDirectory: DocDirectory) {
+    // Thothカスタムタグのリプレース
+    docDirectory.markdowns.foreach{ file =>
+      write(file toOutputFor Markdown(), ThothCustomMarkdownParser(file)) }
 
-  inputDirectories.markdowns.foreach{ file =>
-    write(
-      file toOutputFor Markdown(),
-      ThothCustomMarkdownParser(file))
-  }
-
-  inputDirectories.umls.foreach{ file =>
-    PlantUmlImageGenerator(
-      file,
-      inputDirectories.base/'umls toOutputFor Markdown()) match {
-      case Left(e) => throw e
+    // PlantUML画像出力
+    docDirectory.umls.foreach{ file =>
+      PlantUmlImageGenerator(file, docDirectory.base/'umls toOutputFor Markdown()) match {
+        case Left(e) => throw e
+      }
     }
+
+    // そのままコピーするもの
+    docDirectory.resources.foreach(file => cp(file, file toOutputFor Markdown()))
+
+    // README.mdの出力
+    write(
+      docDirectory.base/"README.md" toOutputFor Markdown(), // TODO 定型的な文字列を固定化する
+      ReadmeGenerator(docDirectory))
+    
+    docDirectory.children.foreach(generateDocuments(_))
   }
-
-  inputDirectories.resources.foreach(file => cp(file, file toOutputFor Markdown()))
-
-  write(
-    inputBaseDir/"README.md" toOutputFor Markdown(), // TODO 定型的な文字列を固定化する
-    ReadmeGenerator(inputDirectories))
 
   implicit class RichPath(path: Path) {
     def toOutputFor(outputType: OutputType)(implicit inputBase: Path, outputBase: Path) = {
@@ -48,10 +52,4 @@ object ThothMain extends App {
 //  alt Officeドキュメント生成フラグ=true
 //  ThothMain --> PandocCommandGenerator : Pandocコマンド生成
 //  PandocCommandGenerator --> ThothMain : Pandocコマンド生成_Response
-//
-//  ThothMain --> Process: コマンド実行
-//  Process --> ThothMain: コマンド実行_Response
-//  end
-//
-//  ThothMain --> ユーザ: 実行_Response
 }
